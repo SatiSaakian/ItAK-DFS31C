8973e009077ad781879b8dc424b959eb5e562bd0
Saakian Sati <s.saakian@it-students.fr>
2024-05-24 10:05:26 +0200

    add:docker-compose

---
+++ my-php-app/src/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+    <h1>welcom!</h1>
+    <p>my firts page Docker!!</p>
+    <img width="300" height="200" src="welcome.png" alt="Welcome Image"/>
+</body>
+</html>
